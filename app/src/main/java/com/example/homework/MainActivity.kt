fa09c354331d011d09299242b9ff38bc1f3ca05f
UucycuHa <93478008+UucycuHa@users.noreply.github.com>
2022-02-07 11:02:33 +0300

    1

--- app/src/main/java/com/example/homework/MainActivity.kt
+++ app/src/main/java/com/example/homework/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.homework
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -9,15 +10,11 @@ import androidx.appcompat.app.AppCompatActivity
 import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
-    private var listUser: MutableList<User> = mutableListOf()
 
-    private val buttonSubmit by lazy {findViewById<Button>(R.id.TapButton)}
-    private val editTextName by lazy {findViewById<EditText>(R.id.InputName)}
-    private val editTextSecondName by lazy {findViewById<EditText>(R.id.InputSecondName)}
-    private val editTextAge by lazy {findViewById<EditText>(R.id.InputAge)}
-    private val textViewUser by lazy {findViewById<TextView>(R.id.textViewUser)}
-    private val textViewList by lazy {findViewById<TextView>(R.id.showList)}
-
+    private val buttonSubmit by lazy { findViewById<Button>(R.id.TupButton) }
+    private val editTextName by lazy { findViewById<EditText>(R.id.InputName) }
+    private val editTextSecondName by lazy { findViewById<EditText>(R.id.InputSecondName) }
+    private val editTextAge by lazy { findViewById<EditText>(R.id.InputAge) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,54 +25,41 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         initView()
     }
-
-    private fun initView (){ buttonSubmit.setOnClickListener{
-        val name = editTextName.text.toString()
-        val secondName = editTextSecondName.text.toString()
-        var age = Int
-    } }
-
-    private fun createUser(name: String, secondName: String, age: Int){
+    private fun createUser (name: String, secondName: String, age: Int) {
         val user = User(name, secondName, age)
-        textViewUser.text = addUser(user)
     }
-    private fun clearField() {
-        editTextName.text.clear()
-        editTextSecondName.text.clear()
-        editTextAge.text.clear()
-        fun showToast() {
-            TODO("Not yet implemented")
-        }
 
-        fun initView()
-        {
-            buttonSubmit.setOnClickListener {
-                val name = editTextName.text.toString()
-                val secondName = editTextSecondName.text.toString()
-                var age: Int
+    private fun initView() {
+        buttonSubmit.setOnClickListener {
+            val name = editTextName.text.toString()
+            val secondName = editTextSecondName.text.toString()
+            var age: Int = 0
 
-                try {
-                    editTextAge.text.toString().toInt().also { age = it }
-                    if (name.isEmpty() || secondName.isEmpty()) showToast() else createUser(name, secondName, age)
-                    clearField()
-                }catch (e: Exception) {
-                    showToast()
-                }
+            try{
+                age = editTextAge.text.toString().toInt()
+                if ( name.isEmpty() || secondName.isEmpty()) showToast() else createUser(name, secondName, age)}
+            catch (e: Exception){
+                showToast()
             }
+
         }
     }
-    private  fun addUser(user: User): String {
 
-        val resultText: String
 
-        if(listUser.contains(user)){
-            resultText = getString(R.string.already_been)
-        } else {
-            resultText = "${user.name} ${user.secondName} ${user.age}"
-            listUser.add(user)
-        }
-        textViewList.text = listUser.toString()
 
-        return resultText
+    private fun showToast() {
+        TODO("Not yet implemented")
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
